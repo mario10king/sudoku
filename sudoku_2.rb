75ca4f71bee1bbdb2300fc6de1b60dbce6fea2e1
Mario Marroqin <mmarroking@gmail.com>
2015-02-01 22:44:17 -0800

    Refactored Solution

--- sudoku_2.rb
+++ sudoku_2.rb
@@ -1,85 +1,92 @@
 class Sudoku
 	
 	def initialize(puzzle)
-		@puzzle = puzzle
-		board 
+		@puzzle = puzzle.split("")
+		create_rows
+		create_columns
+		create_boxes
 	end 
 	
-	def board
-		@puzzle = @puzzle.split("")
-		@game = []
-		@box = []
-		for r in 0 .. 9 
-			@box[r] = []
-		end
-		row = []
-		x = 0
-		@puzzle.each do |digit|
-			row << digit.to_i
-			x += 1
-			if x == 9 
-				@game << row
-			 	x = 0 
-			 	row = []
-			end
-		end
-		for k in 0..8 
-		i = 0
-			while i < 3
-		t = 0
-				while t < 3
-					@box[k] << @game[i + 3*(k/3)][t + 3*(k%3)]
-					t += 1
-				end
-				i += 1
-			end
-		end
-	end
-	
 	def solve!
-		i = 0
-		while i < 9 
+		for i in 0..8
 			x = 0
-			while x < 9
-				if @game[i][x] == 0
+			for x in 0..8
+				if @rows[i][x] == 0
 					for n in 1..9 do
-						if row(n, i) && column(n, x) && box(n, i, x)
-							@game[i][x] = n
-						  solution = Sudoku.new(@game.join.to_s).solve!
+						if check_row(n, i) && check_column(n, x) && check_box(n, i, x)
+							@rows[i][x] = n
+						  solution = Sudoku.new(@rows.join.to_s).solve!
 						  return solution if solution
 						end
 					end
 						return
 				end
-				x += 1
 			end
-			i += 1
 		end
-		 return @game.join.to_s 
+		 return @rows.join.to_s 
 	end
 	
-	def row(digit, row_number)
-		if @game[row_number].include? digit
+	private
+
+
+	def create_rows
+		@rows = []
+		row = []
+		x = 0
+		@puzzle.each do |num|
+			row << num.to_i
+			x += 1
+			if x == 9 
+				@rows << row
+			 	x = 0 
+			 	row = []
+			end
+		end
+	end
+
+	def create_columns
+		@columns = Array.new(9){Array.new}
+		for i in 0..8
+			@rows[i].each_with_index do |val , index|
+				@columns[index] << val
+			end
+		end
+	end
+	
+	def create_boxes
+		@boxes = Array.new(9){Array.new}
+		for i in 0..8 
+			for j in 0..2
+				k = 0
+				for k in 0..2
+					row_index = j + (3 * (i / 3))
+					column_index = k + (3 * (i % 3))			
+					@boxes[i] << @rows[row_index][column_index]
+				end
+			end
+		end
+	end
+
+	def check_row(digit, row_index)
+		if @rows[row_index].include?(digit)
 			return false
 		end
 		return true
 	end
 	
-	def column(digit, column_number)
-		@game.each do |row|
-			if row[column_number] == digit
-				return false
-			end
+	def check_column(digit, column_index)
+		if @columns[column_index].include?(digit)
+			return false
 		end
 		return true
 	end
 	
-	def box(digit, row_number, column_number)
-		if @box[(row_number/3) * 3 + column_number/3].include?(digit)
+	def check_box(digit, row_index, column_index)
+		box_index = (row_index / 3) * 3 + (column_index / 3)
+		if @boxes[box_index].include?(digit)
 			return false
-		else
-			return true
 		end
+		return true
 	end
 end
 
